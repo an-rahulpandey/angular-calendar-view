70580e0dab0c3ffa9a0b3b2cd8eba3da2f95ae64
an-rahulpandey <rahul.p@applicationnexus.com>
2021-12-10 18:38:03 +0530

    Import Safe Style

--- lib/angular-calendar-year-view.service.d.ts
+++
@@ -1,8 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-export declare class AngularCalendarYearViewService {
-    constructor();
-    static ɵfac: ɵngcc0.ɵɵFactoryDeclaration<AngularCalendarYearViewService, never>;
-    static ɵprov: ɵngcc0.ɵɵInjectableDeclaration<AngularCalendarYearViewService>;
-}
-
-//# sourceMappingURL=angular-calendar-year-view.service.d.ts.map
